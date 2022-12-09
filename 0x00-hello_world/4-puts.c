212c316d042aca572591cc61e2eab1dde80be15d
ExcelEdelugo <exceledelugo2w@gmail.com>
2022-12-08 18:01:35 -0800

    @fggh@

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,13 @@
 #include <stdio.h>
 
-/* to output string to std output */
+/**
+ * This is the preferred style for multi-line
+ * comments in C source code.
+ * Please use it consistently.
+ *
+ * Description:  A column of asterisks on the left side,
+ * with beginning and ending almost-blank lines.
+ */
 
 int main(void)
 {
