5f2aa5ec4463dd4cc9d727401e1717b025e8d362
ExcelEdelugo <exceledelugo2w@gmail.com>
2022-12-08 17:23:03 -0800

    finished up till num 6(notdone)

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+/* to output string to std output */
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+/* Printing a string with printf */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
